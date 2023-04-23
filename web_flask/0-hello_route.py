776fb45904455d501c01d9d8e8fab91ab31b0aa0
NatePillay <nathan94pillay@gmail.com>
2023-04-23 10:58:01 +0000

    new commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,10 @@
 from flask import Flask
 app = Flask("__name__")
 
-@app.route('/')
+@app.route('/', stict_slashes=False)
 def hello():
 	"""Return a greeting"""
-	return 'Hello HBNB!'
+	return ("Hello HBNB!")
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000, debug=None)
